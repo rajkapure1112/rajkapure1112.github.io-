c41f77274d4a70b7299e8f531f324a255c22ee3e
Raj Kapure <rajkapure1112@gmail.com>
2024-11-09 03:39:46 +0530

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"af0f0d559c8a87d912a20971bbd84afc80a54b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1173718422"
+    serviceWorkerVersion: "2043411063"
   }
 });
